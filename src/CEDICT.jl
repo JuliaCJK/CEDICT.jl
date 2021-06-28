27a43c32e6b0a027811d30e0d94902349288d267
Tim Lin <32549553+tmthyln@users.noreply.github.com>
2021-06-27 22:15:32 -0400

    ready for release of 0.2.2
    - update CEDICT artifact to latest version (2021-06-27 02:56:13 GMT)
    - update IdiomDragon.jl example to use latest version of Pluto
    - add no-arg `idioms` method + docstring

--- src/CEDICT.jl
+++ src/CEDICT.jl
@@ -10,6 +10,14 @@ idioms
 include("dictionary.jl")
 include("searching.jl")
 
+"""
+    idioms([dict])
+
+Retrieves the set of idioms in the provided dictionary (by looking for a label of "(idiom)" in any
+of the senses) or in the default dictionary if none provided.
+"""
+function idioms end
 idioms(dict) = search_senses(dict, "(idiom)")
+idioms() = idioms(ChineseDictionary())
 
 end
