936c92c87d69286eadb4871359dc596dca997e7f
Tim Lin <32549553+tmthyln@users.noreply.github.com>
2022-12-24 22:35:12 -1000

    docs: fix missing docstring

--- src/dictionary.jl
+++ src/dictionary.jl
@@ -25,22 +25,22 @@ end
 #===============================================================================
 # Dictionary
 ===============================================================================#
+"""
+    ChineseDictionary([filename])
+
+Load a text-based dictionary either from the default dictionary file or from the provided
+filename. The format of the text file must be the same as
+[that used by the CC-CEDICT project](https://cc-cedict.org/wiki/format:syntax) for
+compatibility reasons.
+
+For general use, it's the easiest to just use the default dictionary (from the CC-CEDICT project).
+This is loaded if you don't specify a filename. This dictionary is updated from the official
+project page every so often.
+"""
 struct ChineseDictionary
     entries::Dict{String, Vector{DictionaryEntry}}
     metadata::Dict{String, String}
 
-    """
-        ChineseDictionary([filename])
-
-    Load a text-based dictionary either from the default dictionary file or from the provided
-    filename. The format of the text file must be the same as
-    [that used by the CC-CEDICT project](https://cc-cedict.org/wiki/format:syntax) for
-    compatibility reasons.
-
-    For general use, it's the easiest to just use the default dictionary (from the CC-CEDICT project).
-    This is loaded if you don't specify a filename. This dictionary is updated from the official
-    project page every so often.
-    """
     function ChineseDictionary(filename=joinpath(artifact"cedict", "cedict_ts.u8"))
         dict = Dict{String, Vector{DictionaryEntry}}()
         metadata_dict = Dict{String, String}()
